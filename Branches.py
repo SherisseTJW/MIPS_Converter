61fcc68b99397008447487f060d0d950086c3fdd
SherisseTJW <dev@github.com>
2023-02-02 11:29:14 +0800

    feat: beq / bne if at start of MIPS

---
+++ Branches.py
@@ -0,0 +1,29 @@
+from constants import L_FORMAT_LOOP_OPERATIONS
+from utils import convertToBinaryStr
+
+def convertBranchMIPSToBinary(MIPS_Instruction):
+   for i in range(0, len(MIPS_Instruction)):
+      MIPS_Instruction[i] = MIPS_Instruction[i].replace(",", "").split(" ")
+
+   for key in L_FORMAT_LOOP_OPERATIONS.keys():
+      if key in MIPS_Instruction[0]:
+         return convertLFBranchToBinary(MIPS_Instruction, key)
+   
+def convertLFBranchToBinary(MIPS_Instruction, operation):
+   baseInstruction = MIPS_Instruction[0]
+
+   # Get the immediate value
+   label = baseInstruction[-1] + ":"
+   immediate = 0
+
+   for i in range (0, len(MIPS_Instruction)):
+      if label in MIPS_Instruction[i]:
+         immediate = i - 1
+
+
+   opcode = L_FORMAT_LOOP_OPERATIONS.get(operation)
+   rs = convertToBinaryStr(baseInstruction[2][1:], 5)
+   rt = convertToBinaryStr(baseInstruction[3][1:], 5)
+   immediate = convertToBinaryStr(immediate, 16)
+
+   return opcode + rs + rt + immediate
