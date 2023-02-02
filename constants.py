a81e8ad808b7af68b3c35c869b361db4aa5614da
SherisseTJW <dev@github.com>
2023-02-02 08:40:27 +0800

    feat: Add operations and opcode as constants

---
+++ constants.py
@@ -0,0 +1,22 @@
+
+R_FORMAT_OPERATIONS = {
+   "add": "000000",
+   "addu": "000000",
+   "sub": "000000",
+   "subu": "000000",
+   "sll": "000000",
+   "srl": "000000"
+}
+
+L_FORMAT_OPERATIONS = {
+   "addi": "001000",
+   "addiu": "001001",
+   "beq": "000100",
+   "bne": "000101",
+   "lw": "100011",
+   "sw": "101011"
+}
+
+J_FORMAT_OPERATIONS = {
+   "J": "000010"
+}
